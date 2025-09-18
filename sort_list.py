2768086670c4ac55770b66f3c9bef0fcb67735a1
ANUSHREE <anushreeshivalinga@gmail.com>
2025-09-18 19:27:53 +0530

    Add files via upload

---
+++ sort_list.py
@@ -0,0 +1,44 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        Sort a linked list in O(n log n) time using merge sort.
+        """
+        if not head or not head.next:
+            return head
+
+        # Split the list into two halves
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        mid = slow.next
+        slow.next = None
+
+        # Sort each half
+        left = self.sortList(head)
+        right = self.sortList(mid)
+
+        # Merge sorted halves
+        return self.merge(left, right)
+
+    def merge(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = curr = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        curr.next = l1 if l1 else l2
+        return dummy.next
