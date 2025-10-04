39870830a769efab166512a91f68b1da1fdbe367
ANUSHREE <anushreeshivalinga@gmail.com>
2025-10-04 21:29:47 +0530

    Add files via upload

---
+++ Asteroid Collision.py
@@ -0,0 +1,17 @@
+from typing import List
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack=[]
+        for ast in asteroids:
+            alive =True
+            while stack and ast<0<stack[-1]:
+                if stack[-1]<-ast:
+                    stack.pop()
+                    continue
+                elif stack[-1]==-ast:
+                    stack.pop()
+                alive=False
+                break
+            if alive:
+                stack.append(ast)
+        return stack
